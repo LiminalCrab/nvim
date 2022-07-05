b8a3ec6fcedaea018a46007024ab6b783a1faecb
LiminalCrab <preston@sudogami.com>
2022-07-05 05:40:48 -0400

    Modified files and added lsp configurations.

--- init.lua
+++ init.lua
@@ -1,18 +1,11 @@
 require('mappings')
 require('settings')
+require('lspconfig')
+require('packerconfig')
+require('nvimtreeconfig')
 require('colortheme.gruvbox')
-require('packer_conf')
-require('nvim_tree_conf')
 require('common/telescope')
 require('common/nvim-treesitter')
-
-
--- lsp
-local lspconfig = require("lspconfig")
-local coq = require "coq" -- add this
-require("nvim-lsp-installer").setup {
-  automatic_installation = true
-}
-
-lspconfig.clangd.setup(coq.lsp_ensure_capabilities())
+require('common/lualine')
+require('common/neogit')
 
